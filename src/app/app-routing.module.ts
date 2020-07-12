6288036fd49f09b537f5640410d2c002b395ddb7
saul3d <saul3d@yahoo.com>
2020-07-12 11:53:24 -0500

    removed old properties components and replace them with Rentals and RentalPropertie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent} from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { RentalPropertiesComponent } from './rentalproperties/rentalproperties.component';
 
 
 const routes: Routes = [
   { path:'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'properties', component: PropetiesComponent },
+  { path: 'rentalproperties', component: RentalPropertiesComponent },
   { path: '', component: HomeComponent },
 ];
 
---
+++ src/app/rentals/rental-property/rental-property.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rental-property',
+  templateUrl: './rental-property.component.html',
+  styleUrls: ['./rental-property.component.scss']
+})
+export class RentalPropertyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
