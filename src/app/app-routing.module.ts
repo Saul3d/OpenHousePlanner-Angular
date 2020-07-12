b70565e693c003486baece223337c7bef8bfade9
Saul Solano <saul3d@yahoo.com>
2020-07-11 23:58:05 -0500

    added properties page to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent} from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { PropertiesComponent } from './properties/properties.component';
 
 const routes: Routes = [
   { path:'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'properties', component: PropetiesComponent },
+  { path: 'properties', component: PropertiesComponent },
   { path: '', component: HomeComponent },
 ];
 
